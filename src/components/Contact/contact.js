5fc25d00e8e3d6311a409387cdbb93885331b2e9
Kelsey Benedict <kelseybenedict26@gmail.com>
2021-06-05 23:41:16 -0600

    added resume and made it downloadable on contact page

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,24 +1,57 @@
 import React from "react";
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import IconButton from '@material-ui/core/IconButton';
+import { makeStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
+import DescriptionIcon from '@material-ui/icons/Description';
+import Resume from "../../images/resumeUpdated.pdf";
 
-function Contact (){
-    return(
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+function Contact() {
+    const classes = useStyles();
+    return (
         <>
-        <section className="container">
-          <div className="row">
-            <div className="col d-flex justify-content-center">
-              <div className="card">
-                <div className="card-body">
-                  <h5 className="card-title">Email:</h5>
-                  <p className="card-text">kelseybenedict26@gmail.com</p>
-                  <h5 className="card-title">Phone:</h5>
-                  <p className="card-text">(503) 990-2739</p>
-                  <a href="https://github.com/kelseybenedict" target="_blank" rel="noopener noreferrer"><h5 className="card-title">GitHub</h5></a>
-                  <a href="https://www.linkedin.com/in/kelsey-benedict-8396581a2/" target="_blank" rel=" noopener noreferrer"><h5 className="card-title">LinkedIN</h5></a>
+            <section className="container">
+                <div className="row">
+                    <div className="col d-flex justify-content-center">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Email:</h5>
+                                <p className="card-text">kelseybenedict26@gmail.com</p>
+                                <h5 className="card-title">Phone:</h5>
+                                <p className="card-text">(503) 990-2739</p>
+                                <Link href="https://github.com/kelseybenedict">
+                                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="github">
+                                    <GitHubIcon />
+                                </IconButton>
+                                </Link>
+                                <Link href="https://www.linkedin.com/in/kelsey-benedict-8396581a2/">
+                                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="linkedin">
+                                    <LinkedInIcon />
+                                </IconButton></Link>
+                                <Link href={Resume} target="_blank" download>
+                                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="resume">
+                                    <DescriptionIcon />
+                                </IconButton>
+                                </Link>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </div>  
-        </section>
+            </section>
         </>
     )
 }
--- src/components/Nav/navbar.js
+++ src/components/Nav/navbar.js
@@ -4,9 +4,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
@@ -37,12 +34,6 @@ function NavBar() {
                     <Link to="/portfolio"><Button color="inherit">Portfolio</Button></Link>
                     <Link to="/contact"><Button color="inherit">Contact</Button></Link>
 
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="github">
-                        <GitHubIcon />
-                    </IconButton>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="linkedin">
-                        <LinkedInIcon />
-                    </IconButton>
                 </Toolbar>
             </AppBar>
         </div>
